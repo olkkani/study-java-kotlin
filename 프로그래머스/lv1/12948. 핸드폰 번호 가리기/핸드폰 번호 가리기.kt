6dead6b5bbd8d6e0703ecb42bea6a8a79ffdb539
Chanjin Ahn <97466353+olkkani@users.noreply.github.com>
2022-10-04 15:02:30 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 10.29 ms, Memory: 63.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(phone_number: String): String {
+        var answer = ""
+        for (i in 1..phone_number.length - 4) {
+            answer += "*"
+        }
+        for (i in phone_number.length-4 until phone_number.length){
+            answer += phone_number[i]
+        }
+
+        return answer
+    }
+}
