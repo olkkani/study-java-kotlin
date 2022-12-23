75dd270d52dc71b2781ba68d842bb8108c473897
chanjin_ahn <olkkani@gmail.com>
2022-12-23 16:32:09 +0900

    :sparkles: Add Singleton example

---
+++ app/src/main/kotlin/io/olkkani/start/other/Singleton.kt
@@ -0,0 +1,53 @@
+package io.olkkani.start.other
+
+import java.time.LocalDateTime
+
+object Singleton {
+
+    val a = 1234
+
+    fun printA() = print(a)
+}
+
+object DatetimeUtils {
+    val now : LocalDateTime
+        get() = LocalDateTime.now()
+
+    const val DEFAULT_FORMAT = "YYYY-MM-DD"
+
+    fun same(a: LocalDateTime, b: LocalDateTime): Boolean {
+        return a == b
+    }
+}
+// 동반 객체
+class MyClass {
+    private constructor()
+    companion object {
+        val a = 1234
+
+        fun newInstance() = MyClass()
+    }
+}
+
+fun main () {
+    // singleton example - 1
+    println(Singleton.a)
+    Singleton.printA()
+
+
+    println(DatetimeUtils.now)
+    println(DatetimeUtils.now)
+    println(DatetimeUtils.now)
+
+    println(DatetimeUtils.DEFAULT_FORMAT)
+
+    val now = LocalDateTime.now()
+    println(DatetimeUtils.same(now, now))
+
+    //
+    println(MyClass.a)
+    println(MyClass.Companion.a) // 특별한 이유가 업다면 companion 은 생략이 가능
+    println(MyClass.newInstance())
+    println(MyClass.Companion.newInstance())
+
+}
