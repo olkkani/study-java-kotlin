51a9e3b5d8aae17e292ff2cc2b7c6462946e9b6a
chanjin_ahn <olkkani@gmail.com>
2022-12-29 20:59:27 +0900

    :sparkles: Add sealedClass

---
+++ app/src/main/kotlin/io/olkkani/start/exampleclass/SealedClass.kt
@@ -0,0 +1,66 @@
+package io.olkkani.start.exampleclass
+
+import org.intellij.lang.annotations.Language
+
+sealed class Developer {
+    abstract val name: String
+    abstract fun code(language: Language)
+}
+
+data class BackendDeveloper(override val name: String) : Developer() {
+    override fun code(language: Language) {
+        println("저는 백엔드 개발자입니다. ${language}를 사용합니다.")
+    }
+
+}
+
+data class FrontendDeveloper(override val name: String) : Developer() {
+    override fun code(language: Language) {
+        println("저는 프론트엔드 개발자입니다. ${language}를 사용합니다.")
+    }
+}
+
+object OtherDeveloper : Developer() {
+    override val name: String = "익명"
+
+    override fun code(language: Language) {
+        TODO("Not yet implemented")
+    }
+}
+
+data class AndroidDeveloper(override val name: String) : Developer() {
+    override fun code(language: Language) {
+        println("저는 안드로이드 개발자입니다. ${language}를 사용합니다.")
+    }
+}
+
+
+object DeveloperPool {
+    val pool = mutableMapOf<String, Developer>()
+
+    fun add(developer: Developer) = when(developer){
+        is BackendDeveloper -> pool[developer.name] = developer
+        is FrontendDeveloper -> pool[developer.name] = developer
+        is AndroidDeveloper -> pool[developer.name] = developer
+        is OtherDeveloper -> println("지원하지않는 개발자종류입니다.")
+//        else -> {
+//            println("지원하지않는 개발자입니다.")
+//        }
+    }
+    fun get(name: String) = pool[name]
+}
+
+fun main() {
+    val backendDeveloper = BackendDeveloper(name="토니")
+    DeveloperPool.add(backendDeveloper)
+
+    val frontendDeveloper = FrontendDeveloper(name="카즈야")
+    DeveloperPool.add(frontendDeveloper)
+
+    val androidDeveloper = FrontendDeveloper(name="안드로")
+    DeveloperPool.add(androidDeveloper)
+
+    println(DeveloperPool.get("토니"))
+    println(DeveloperPool.get("카즈야"))
+    println(DeveloperPool.get("안드로"))
+}
